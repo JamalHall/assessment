f641b94006dcd4021143bcd053c07342d43cf0ba
Sina Rafieefar <anisr95@yahoo.com>
2022-08-27 22:54:54 -0700

    Add Mobile responsiveness for small devices (350px)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('And this is where the Javascript goes')
+
+const headerImage = document.querySelector("#header-image")
+const images = ["./assets/banner_image_1.png","./assets/banner_image_2.png"]
+var counter = 0;
+
+headerImage.src = images[1];
+setInterval(() => {
+    // console.log("Hi")
+}, 1000);
