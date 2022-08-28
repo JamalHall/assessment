bb6bddf1b5fbb5d5ea2ec0d06ed1ab48bd7001e0
Sina Rafieefar <anisr95@yahoo.com>
2022-08-28 02:04:41 -0700

    Add Modal and Backdrop when Parent Portal button is clicked

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 console.log('And this is where the Javascript goes')
 
-const headerImage = document.querySelector("#header-image")
-const images = ["./assets/banner_image_1.png","./assets/banner_image_2.png"]
-var counter = 0;
+const portalButton = document.querySelector("button.portal")
+const modalClose = document.querySelector("#modal-close")
+const backdrop = document.querySelector(".backdrop")
+const modal = document.querySelector(".modal")
 
-headerImage.src = images[1];
-setInterval(() => {
-    // console.log("Hi")
-}, 1000);
+const headerImage = document.querySelector("#header-image")
+const image1 = document.querySelector("#header-image1");
+const image2 = document.querySelector("#header-image2");
+// const images = ["./assets/banner_image_1.png","./assets/banner_image_2.png"]
+let counter = 0;
+// let imageWidth = image2.clientWidth;
+// console.log(imageWidth)
+
+
+portalButton.addEventListener("click", () => {
+    backdrop.style.display = "block"
+    modal.style.display = "flex"
+});
+
+modalClose.addEventListener("click", () => {
+    backdrop.style.display = "none"
+    modal.style.display = "none"
+})
+
+backdrop.addEventListener("click", () => {
+    backdrop.style.display = "none"
+    modal.style.display = "none"
+})
+
+
+// setInterval(() => {
+//     if(counter === 0) {
+//         // headerImage.src = images[counter];
+//         image2.style.transform = `translateX(${imageWidth}px)`;
+//         counter++;
+//     } else {
+//         // headerImage.src = images[counter];
+//         image2.style.transform = `translateX(${imageWidth}px)`;
+//         counter = 0;
+//     }
+// }, 5000);
