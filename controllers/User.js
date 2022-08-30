9b39d3fb873f92f51a385868bd4a2584d4d49479
Sina Rafieefar <anisr95@yahoo.com>
2022-08-30 02:16:54 -0700

    Add comment to User Controller file

--- controllers/User.js
+++ controllers/User.js
@@ -1,18 +1,22 @@
 import User from "../models/User.js";
 import ExpressError from "../utils/ExpressError.js";
-import session from "express-session";
+// import session from "express-session";
 
 
 export const signup = async (req, res, next) => {
     try {
         const {userEmail, userName, userPassword} = req.body;
 
+        // Check to see if user already exists
         const existingUser = await User.findOne({email: userEmail.toLowerCase()});
 
+        // If user already exists, throws an error
         if(existingUser) throw new ExpressError("User Already Exists", 404)
 
+        // Throws error if user submits an empty password
         if(userPassword.length === 0) throw new ExpressError("Password cannot be empty", 400);
 
+        // A new user is created and saved to MongoDB
         const newUser = new User({
             name: userName,
             password: userPassword,
@@ -22,11 +26,6 @@ export const signup = async (req, res, next) => {
         await newUser.save();
 
         req.session.user_id = newUser._id;
-
-        console.log("Here is your info: ", req.body);
-        console.log(userEmail)
-        console.log(userName)
-        console.log(userPassword)
 
         res.redirect('/parentPortal');
     } catch (error) {
@@ -39,20 +38,17 @@ export const login = async (req, res, next) => {
         const {userEmail, userName, userPassword} = req.body;
         const user = await User.findOne({email: userEmail.toLowerCase()});
 
+        // If user can't be find an error will be thrown
         if(!user) throw new ExpressError("User Not Found", 404);
 
+        // If entered password and the password saved in database don't match and error will be thrown
+        // Passwords must be hashed in future updates for security improvement
         const isPasswordCorrect = userPassword === user.password;
-
         if(!isPasswordCorrect) throw new ExpressError("Password is incorrect", 401);
 
         req.session.user_id = user._id;
- 
-        console.log(req.body)
-        // res.sendFile("../parentPortal.html");
-        // console.log(__dirname);
-        // res.render('../parentPortal.html');
+
         res.redirect('/parentPortal');
-        // res.send("User Found");
     } catch(error){
         next(error)
     }
